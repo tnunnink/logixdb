4ffa2683c5c850184d8b6e051ccb87bbfda33f28
tnunnink <timothyjnunnink@gmail.com>
2026-02-26 21:06:04 -0600

    Update SQL migration to conditionally create login and user for service account only on Windows platforms. Adjust IDE workspace settings.

--- src/LogixDb.Data.SqlServer/Migrations/M001SqlServerConfigureServiceAccount.cs
+++ src/LogixDb.Data.SqlServer/Migrations/M001SqlServerConfigureServiceAccount.cs
@@ -11,27 +11,34 @@ public class M001SqlServerConfigureServiceAccount : Migration
 {
     public override void Up()
     {
-        // 1. Create Login for the Virtual Account (Server level)
+        // Check if we are running on Windows. 
+        // sys.dm_os_host_info is available in SQL Server 2017+
         Execute.Sql(
             """
-            IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = 'NT SERVICE\LogixDb')
+            DECLARE @isWindows BIT = 0;
+            IF EXISTS (SELECT 1 FROM sys.dm_os_host_info WHERE host_platform = 'Windows')
             BEGIN
-                DECLARE @sql NVARCHAR(MAX);
-                SET @sql = N'CREATE LOGIN [NT SERVICE\LogixDb] FROM WINDOWS WITH DEFAULT_DATABASE = ' + QUOTENAME(DB_NAME());
-                EXEC sp_executesql @sql;
+                SET @isWindows = 1;
             END
-            """
-        );
 
-        // 2. Create User and Grant Roles (Database level)
-        Execute.Sql(
-            """
-            IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'LogixServiceUser')
+            IF @isWindows = 1
             BEGIN
-                CREATE USER [LogixServiceUser] FOR LOGIN [NT SERVICE\LogixDb];
-                ALTER ROLE [db_datareader] ADD MEMBER [LogixServiceUser];
-                ALTER ROLE [db_datawriter] ADD MEMBER [LogixServiceUser];
-                ALTER ROLE [db_owner] ADD MEMBER [LogixServiceUser];
+                -- 1. Create Login for the Virtual Account (Only on Windows)
+                IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = 'NT SERVICE\LogixDb')
+                BEGIN
+                    DECLARE @sql NVARCHAR(MAX);
+                    SET @sql = N'CREATE LOGIN [NT SERVICE\LogixDb] FROM WINDOWS WITH DEFAULT_DATABASE = ' + QUOTENAME(DB_NAME());
+                    EXEC sp_executesql @sql;
+                END
+
+                -- 2. Create User and Grant Roles
+                IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'LogixServiceUser')
+                BEGIN
+                    CREATE USER [LogixServiceUser] FOR LOGIN [NT SERVICE\LogixDb];
+                    ALTER ROLE [db_datareader] ADD MEMBER [LogixServiceUser];
+                    ALTER ROLE [db_datawriter] ADD MEMBER [LogixServiceUser];
+                    ALTER ROLE [db_owner] ADD MEMBER [LogixServiceUser];
+                END
             END
             """
         );
@@ -39,7 +46,16 @@ public class M001SqlServerConfigureServiceAccount : Migration
 
     public override void Down()
     {
-        Execute.Sql("DROP USER [LogixServiceUser];");
-        Execute.Sql("DROP LOGIN [NT SERVICE\\LogixDb];");
+        Execute.Sql(
+            """
+            IF EXISTS (SELECT 1 FROM sys.dm_os_host_info WHERE host_platform = 'Windows')
+            BEGIN
+                IF EXISTS (SELECT * FROM sys.database_principals WHERE name = 'LogixServiceUser')
+                    DROP USER [LogixServiceUser];
+                IF EXISTS (SELECT * FROM sys.server_principals WHERE name = 'NT SERVICE\LogixDb')
+                    DROP LOGIN [NT SERVICE\LogixDb];
+            END
+            """
+        );
     }
 }
